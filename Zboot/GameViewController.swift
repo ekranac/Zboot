bbfccb5d995ceb23ed53a270b3becd4a170ef914
Ziga Besal <besal.ziga@gmail.com>
2017-05-03 18:53:02 +0200

    Added pan gesture recognizer

--- Zboot/GameViewController.swift
+++ Zboot/GameViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    @IBOutlet weak var bucket: UILabel!
+    
+    fileprivate var gameIsActive: Bool!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        gameIsActive = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +29,24 @@ class GameViewController: UIViewController {
         return true
     }
 
+    @IBAction func moveBucket(_ sender: UIPanGestureRecognizer) {
+        if !gameIsActive {
+            return
+        }
+        let translation = sender.translation(in: self.view)
+        guard let senderView = sender.view, let rootView = senderView.superview else {
+            print("Could not retrieve view")
+            return
+        }
+        let currentOriginPositionX = senderView.frame.origin.x
+        let newOriginPositionX = currentOriginPositionX + translation.x
+        
+        if newOriginPositionX > 0.0 && (newOriginPositionX+senderView.frame.width) < rootView.frame.width {
+            senderView.center = CGPoint(x: senderView.center.x+translation.x, y: senderView.center.y)
+            sender.setTranslation(CGPoint.zero, in: self.view)
+        }
+
+    }
 
 }
 
